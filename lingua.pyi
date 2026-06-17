f8fa6772803ac9f9141dff38765daacfda0da7ed
Meiyazhagan Shanmugam <meiyazhagan.s+fwinc@freshworks.com>
2026-06-17 19:11:44 +0530

    test commit

--- lingua.pyi
+++ lingua.pyi
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from enum import Enum
 from pathlib import Path
 from typing import FrozenSet, Optional, List, Set
